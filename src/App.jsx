0196de5cb7f63e230edb44490284181b69a5cc35
doyinsola-coder <doyinspace@gmail.com>
2026-08-03 13:23:33 +0100

    Add ScrollToTop component to reset scroll position on route change

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,13 @@ import Login from './pages/Login'
 import Dashboard from './pages/Dashboard'
 import AdminDashboard from './pages/AdminDashboard'
 import MainLayout from './layout';
-
+import ScrollToTop from './components/ScrollToTop';
 
 
 function App() {
   return (
     <BrowserRouter>
+     <ScrollToTop />
       <Routes>
 
         {/* Routes with Navbar and Footer */}
